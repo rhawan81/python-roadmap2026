4ddd6409bd902b11246ecb00532a3e87cebff41a
Michel <rhawanjs@gmail.com>
2026-01-06 12:00:14 -0300

    Projeto CLI com POO em Python

---
+++ dia14.py
@@ -0,0 +1,64 @@
+class Item:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def exibir(self):
+        return f"Nome: {self.nome} | Idade: {self.idade}"
+
+
+
+
+
+
+class Sistema:
+    def __init__(self):
+        self.items = []
+        
+    def menu(self):
+        while True:
+            print("""1.Mostrar Menu
+    2.Cadastrar Item
+    3. Listar Items
+    4.Sair""")
+            try:
+                opcao = int(input('Escolha oque deseja fazer'))
+            except ValueError:
+                print('valor invalido tente novamente')
+            if opcao == 1:
+                continue
+            elif opcao == 2:
+                self.cadastrar_item()
+            elif opcao == 3:
+                self.listar_items()
+            elif opcao == 4:
+                 print('saindo do sistema')
+                 break
+               
+                
+    def cadastrar_item(self):
+        nome = input('Nome: ').strip()
+        while True:
+            try: 
+                idade = int(input('Idade:'))
+                break
+            except ValueError:
+                print('idade invalida . digite um numero')
+                
+        item = Item(nome,idade)
+        self.items.append(item)
+    
+        print(f'Item {item.nome} cadastrado com sucesso !')
+        
+    def listar_items(self):
+        if not self.items:
+            print('Nenhum item cadastrado ainda ')
+            return
+        print("\n=== ITENS CADASTRADOS ===")
+        for i , item in enumerate(self.items,start=1):
+            print(f'{i} . {item.exibir()}')
+        
+
+if __name__ == "__main__":
+    sistema = Sistema()
+    sistema.menu()
