471389e90e1a8d0eff28ee8fa9e34627bf707e29
Michel <rhawanjs@gmail.com>
2026-01-15 20:56:50 -0300

    Adiciona validações para os campos nome e idade na classe Usuario

--- api_fastapi/main.py
+++ api_fastapi/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel ,Field,validator
 usuarios_cadastrados = []
 
 class Usuario(BaseModel):
-    nome: str
-    idade:int
-    
+    nome: str = Field(...,min_length = 1) # usado para fazer validaçoes  e metadados do campo os tres pontos no começo , e essa string precisa ter pelo menos 1 caractere significa que o campo é obrigatorio 
+    idade:int = Field(..., gt = 0) # mesma coisa se aplica para este campo onde se a idade for negativa a API ira retornar um erro 
+    ## caso o usuario digite um nome vazio  a api ira retornar erro 
+    @validator('nome')
+    def nome_vazio(cls,value):
+        if value.strip() == "":
+            raise ValueError('Nome nao pode ser vazio')
+        
+        return value
 
 app = FastAPI()
 
