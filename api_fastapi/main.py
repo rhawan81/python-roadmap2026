a5fccf2a1e933bcdb39e8c6a31f1ec869eb8b62e
Michel <rhawanjs@gmail.com>
2026-01-19 19:39:47 -0300

    Refatora a classe Usuario para UsuarioCreate, adiciona validação de nome e ajusta endpoints da API para cadastro e listagem de usuários

--- api_fastapi/main.py
+++ api_fastapi/main.py
@@ -1,40 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel ,Field,validator
 usuarios_cadastrados = []
 
-class Usuario(BaseModel):
-    nome: str = Field(...,min_length = 1) # usado para fazer validaçoes  e metadados do campo os tres pontos no começo , e essa string precisa ter pelo menos 1 caractere significa que o campo é obrigatorio 
+class UsuarioCreate(BaseModel):
+    nome: str = Field(...,min_length = 1 , strip_whitespace=True) # usado para fazer validaçoes  e metadados do campo os tres pontos no começo , e essa string precisa ter pelo menos 1 caractere significa que o campo é obrigatorio 
     idade:int = Field(..., gt = 0) # mesma coisa se aplica para este campo onde se a idade for negativa a API ira retornar um erro 
     ## caso o usuario digite um nome vazio  a api ira retornar erro 
-    @validator('nome')
-    def nome_vazio(cls,value):
-        if value.strip() == "":
-            raise ValueError('Nome nao pode ser vazio')
-        
-        return value
+
+    
+class UsuarioOut(BaseModel):
+    id: int
+    nome: str
+    idade : int
 
 app = FastAPI()
 
 @app.get("/")
 def exibir():
-    return {f'API FUNCIONANDO CORRETAMENTE ! '}
-@app.post('/usuario')
-def cadastrar_user(dados: Usuario):
-    novo_usuario = {'id': len(usuarios_cadastrados) + 1 , "nome": dados.nome , 'idade':dados.idade}
+    return {"status": "ok", "message":"API funcionando corretamente"}
+@app.post('/users', response_model=UsuarioOut, status_code=201)
+def cadastrar_user(dados: UsuarioCreate):
+    novo_usuario = {'id': 
+        len(usuarios_cadastrados) + 1 , 
+        "nome": dados.nome , 
+        'idade':dados.idade}
     usuarios_cadastrados.append(novo_usuario)
-    return {'Mensagem': 'Usuario Criado com sucesso', 'Usuario:': novo_usuario}
+    return novo_usuario
 
 
 
-@app.get('/users_cadastrados')
+@app.get('/users', response_model=list[UsuarioOut])
 def mostrar_cadastros():
-    return {'Mensagem:' 'Usuarios cadastrados': usuarios_cadastrados}
+   return usuarios_cadastrados
     
-    
-@app.delete("/deletar_user/{id}")
+@app.delete("/users/{id}", status_code=204)
 def deletar_user(id:int):
     for p, item in enumerate(usuarios_cadastrados):
         if item['id'] == id:
             usuarios_cadastrados.pop(p)
-            return {'mensagem': 'Usuario  removido com sucesso'}
-    return {'mensagem': 'Usuario nao foi encontrado'}
+            return 
+    raise HTTPException(status_code = 404 , detail= "Usuario nao foi encontrado")
